5155ca0f35998ceba6fd4b0a697f0889f1d30260
yuanyu90221 <yuanyu90221@gmail.com>
2019-03-24 21:59:32 +0800

    加入判斷是否 做出source map

--- webpack.config.js
+++ webpack.config.js
@@ -13,22 +13,25 @@ let config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
+      entry: './src/index.html',
       filename: `${path.join(__dirname,'dist','index.html')}`,
       template: './src/index.html',
       title: 'My App'
     })
   ]
 }
-module.exports = (env, argv ) =>{
+module.exports = (env, argv) =>{
+  // 根據 是否有 --mode=production來判別是否要compile成 production
   if (argv.mode && (argv.mode === 'production' || argv.mode === 'development' )) {
     config.mode = argv.mode;
   } else {
     config.mode = 'development';
   }
+  // 設定是否有 source map https://webpack.docschina.org/configuration/devtool/
   if (config.mode==='development') {
     config.devtool = 'source-map';
-  } else {
-    config.devtool = null;
+  } else { 
+    config.devtool = 'nosources-source-map';
   }
   return config;
 };
