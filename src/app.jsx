8c690f188963d894eb16b023f7c030d5f087623b
JsonLiang <json@cbx.io>
2019-07-09 21:12:29 +0800

    update

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // create a DOM
-let element = (<h1>First React!</h1>);
+let element = (<h1>First React stats!</h1>);
 
 // render the element into app
 ReactDOM.render(
